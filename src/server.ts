3738731bc993dab92414dc7c0589de5bc7206008
Firellon <macfirellon@gmail.com>
2021-03-18 00:15:02 +0300

    add: tslog

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
+import { Logger } from 'tslog'
 import express from 'express'
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// Logger
+const logger = new Logger({ name: 'Nodockernetes'})
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  console.log(`GET /`)
+  logger.info(`GET /`)
   res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+logger.info(`Running on http://${HOST}:${PORT}`);
